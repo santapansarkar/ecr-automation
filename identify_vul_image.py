b54b349a695057b3fe60e362c279984b4d1b57f4
Sarkar <sxsarkar@b0be835c9e13.ant.amazon.com>
2024-08-20 17:55:21 +0530

    added

--- identify_vul_image.py
+++ identify_vul_image.py
@@ -1,38 +1,51 @@
 import json
 import boto3
 
-# Initialize the AWS clients
-ecr = boto3.client('ecr')
-events = boto3.client('events')
+def aws_con():
+    ecr_client = boto3.client('ecr', region_name='ap-south-1')
+    return ecr_client
 
-def lambda_handler(event, context):
-    # Extract the image details from the event
-    image_digest = event['detail']['image-digest']
-    repository_name = event['detail']['repository-name']
-    registry_id = event['detail']['registry-id']
 
+def vul_iamge(**vul_image_kwargs):
     # Get the image scan findings
-    response = ecr.describe_image_scan_findings(
-        registryId=registry_id,
-        repositoryName=repository_name,
-        imageDigest=image_digest
-    )
-
+    response = vul_image_kwargs['ecr_client'].describe_image_scan_findings(
+                registryId=vul_image_kwargs['registry_id'],
+                repositoryName=vul_image_kwargs['repository_name'],
+                imageId={
+                    'imageDigest': vul_image_kwargs['image_digest'],
+                    'imageTag': vul_image_kwargs['image_tag']
+                },
+            )
+    #print(response['imageScanFindings']['findingSeverityCounts'])
+    resource_arn = response['imageScanFindings']['enhancedFindings'][0]['resources'][0]['id']
+    print(resource_arn)
     # Check if any vulnerabilities were found
     vulnerabilities_found = False
-    for finding in response['imageScanFindings']['findings']:
-        if finding['severity'] in ['CRITICAL', 'HIGH', 'MEDIUM']:
-            vulnerabilities_found = True
+    for finding in response['imageScanFindings']['findingSeverityCounts']:
+            if finding in ('CRITICAL','HIGH'):
+                 vulnerabilities_found = True
+                 tag_vul_images(resource_arn)
             break
 
     if vulnerabilities_found:
         # Prevent the image from being downloaded
-        print(f"Vulnerabilities found in {repository_name}:{image_digest}. Blocking download.")
+        print(f"Vulnerabilities found in {vul_image_kwargs['repository_name']}:{vul_image_kwargs['image_digest']}. Blocking download.")
 
         # Optionally, you can send a notification or take additional actions here
-        send_notification(repository_name, image_digest)
+        #send_notification(repository_name, image_digest)
     else:
-        print(f"No vulnerabilities found in {repository_name}:{image_digest}. Allowing download.")
+        print(f"No vulnerabilities found in {vul_image_kwargs['repository_name']}:{vul_image_kwargs['image_digest']}. Allowing download.")
+
+def tag_vul_images(resource_arn):
+     response = ecr_client.tag_resource(
+                    resourceArn=resource_arn,
+                    tags=[
+                        {
+                            'Key': 'DEPLOY',
+                            'Value': 'NO'
+                        },
+                    ]
+                )        
 
 def send_notification(repository_name, image_digest):
     # Code to send a notification (e.g., AWS SNS, email, etc.)
@@ -42,3 +55,23 @@ def send_notification(repository_name, image_digest):
     subject = 'Vulnerable ECR Image Detected'
     message = f"Vulnerabilities found in ECR image {repository_name}:{image_digest}. Download blocked."
     sns.publish(TopicArn=topic_arn, Subject=subject, Message=message)
+
+
+if __name__ == "__main__":
+    ecr_client = aws_con()
+    image_digest = 'sha256:3109bef765bdfc6dd8efc1e649bd61141f67a50e0860c1a54847228264328650'
+    repository_name = 'on-demand-scan'
+    registry_id = '328753010123'
+    image_tag = 'dev1'
+    vul_image_kwargs = {
+        'ecr_client': ecr_client,
+        'image_digest': image_digest,
+        'repository_name': repository_name,
+        'registry_id' : registry_id,
+        'image_tag' : image_tag
+    }
+    vul_iamge(**vul_image_kwargs)
+    vul_tag_kwargs = {
+         'ecr_client': ecr_client,
+         'resource_arn':'resource_arn' 
+    }
